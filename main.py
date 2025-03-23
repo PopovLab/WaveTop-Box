8d379a489c712816b61e77b09f44d83391a66c57
Alex <temper8@gmail.com>
2025-03-23 11:40:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 import WaveBox.App as App
-import WaveBox.History as History
+import WaveBox.history as history
 
 if __name__ == '__main__':
     
-    App.work_space_loc = History.get_last()
+    App.work_space_loc = history.get_last()
     
     while App.live:
         App.run()
